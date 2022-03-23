efb22196b8c89fc04b7b9f38db5fee557f6db684
PradeepKumaresn <101967792+PradeepKumaresn@users.noreply.github.com>
2022-03-23 16:57:57 +0600

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+package Week1Day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int fac=1;
+		
+		for(int i=5; i>0; ++i)
+		{
+			fac=fac*i;
+
+			
+		}
+		System.out.println(fac);
+
+
+
+	}
+
+}
